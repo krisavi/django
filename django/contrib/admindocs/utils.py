f7ff7d149ec5b8d278414ef7229a97945d371368
Kris Avi <kris@krisavi.com>
2016-10-31 16:05:45 +0200

    Documentation links generation rehaul

--- django/contrib/admindocs/utils.py
+++ django/contrib/admindocs/utils.py
@@ -1,10 +1,16 @@
 "Misc. utility functions/classes for admin documentation generator."
 
 import re
+from importlib import import_module
+
 from email.errors import HeaderParseError
 from email.parser import HeaderParser
 
+from django.apps import apps
+from django.conf import settings
+from django.core.exceptions import ViewDoesNotExist
 from django.urls import reverse
+from django.utils import six
 from django.utils.encoding import force_bytes
 from django.utils.safestring import mark_safe
 
@@ -102,17 +108,43 @@ ROLES = {
 
 
 def create_reference_role(rolename, urlbase):
+    def _get_full_name(func):
+        mod_name = func.__module__
+        if six.PY3:
+            return '%s.%s' % (mod_name, func.__qualname__)
+        else:
+            # PY2 does not support __qualname__
+            func_name = getattr(func, '__name__', func.__class__.__name__)
+            return '%s.%s' % (mod_name, func_name)
+
     def _role(name, rawtext, text, lineno, inliner, options=None, content=None):
         if options is None:
             options = {}
         if content is None:
             content = []
+        text = text.replace(':', '.')   # This is for namespace : to . conversion for link URL and link text.
+        link_text = text.lower()        # Link URL string from lower-case text, no changes on template
+        if '/views/' in urlbase:        # Views override and check, changes link URL and link text
+            urlconf = import_module(settings.ROOT_URLCONF)
+            view_functions = extract_views_from_urlpatterns(urlconf.urlpatterns)
+            for (func, regex, namespace, func_name) in view_functions:
+                if link_text in ('.'.join((namespace or []) + (func_name and [func_name] or []))):
+                    link_text = _get_full_name(func)
+                    text = text.replace(func_name, func.func_name)
+        elif '/models/' in urlbase:     # Models override, changes link URL to correct
+                                        # format according to model from lowercase text.
+            m_list = [m._meta for m in apps.get_models()]
+            for m in m_list:
+                if link_text in ('.'.join(([m.app_label] or []) + ([m.model_name] or []))):
+                    link_text = m.label
+        elif '/#' in urlbase:           # Filters and tag override for link text
+            text = text.split('-', 1)[1]
         node = docutils.nodes.reference(
             rawtext,
             text,
             refuri=(urlbase % (
                 inliner.document.settings.link_base,
-                text.lower(),
+                link_text,              # Link URL string
             )),
             **options
         )
@@ -142,3 +174,32 @@ if docutils_is_available:
 
     for name, urlbase in ROLES.items():
         create_reference_role(name, urlbase)
+
+
+def extract_views_from_urlpatterns(urlpatterns, base='', namespace=None):
+    """
+    Return a list of views from a list of urlpatterns.
+
+    Each object in the returned list is a two-tuple: (view_func, regex)
+    """
+    views = []
+    for p in urlpatterns:
+        if hasattr(p, 'url_patterns'):
+            try:
+                patterns = p.url_patterns
+            except ImportError:
+                continue
+            views.extend(extract_views_from_urlpatterns(
+                patterns,
+                base + p.regex.pattern,
+                (namespace or []) + (p.namespace and [p.namespace] or [])
+            ))
+        elif hasattr(p, 'callback'):
+            try:
+                views.append((p.callback, base + p.regex.pattern,
+                              namespace, p.name))
+            except ViewDoesNotExist:
+                continue
+        else:
+            raise TypeError(_("%s does not appear to be a urlpattern object") % p)
+    return views
